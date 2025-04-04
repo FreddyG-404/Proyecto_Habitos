22e70ce46cef6eeda6514dca9a1eb87b8c9016dd
Santiago G <124182883+FreddyG-404@users.noreply.github.com>
2025-04-04 17:33:47 -0500

    Delete backend directory

--- backend/src/main/java/investigacionjwt/appjwt/Controller/AuthRequestDto.java
+++
@@ -1,9 +0,0 @@
-package investigacionjwt.appjwt.Controller;
-
-public record AuthRequestDto(
-        String nombre,
-        String username,
-        String password,
-        String email) {
-
-}
